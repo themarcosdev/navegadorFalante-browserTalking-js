df21e54263bf3b5fdbe0570bd715be8263b96eb9
themarcosdev <>
2021-07-31 06:52:59 +0000

    criação de input e extensão do chrome

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+
+
 //usando metodo para pegar todas as vozes e filtramos em pt-br
 const voice = speechSynthesis.getVoices().filter((voice)=>voice.lang.includes("pt-BR"))[0]// isso retorna um array e quero apenas um elemento.
 
 // criando a função de play e def que irá pegar o está no documento com o id fala
 function play(){
-  const fala = document.getElementById("fala").innerText
+  //recebendo o input do id=texto
+  const input = document.querySelector("#texto");
+  
+  // definindo que a const fala é o que recebemos do input;
+  const fala = input.value;
+
   //criando o utterance que é um parametro para a nossa api
   const utterance = new SpeechSynthesisUtterance(fala);
   
@@ -13,3 +20,4 @@ function play(){
   speechSynthesis.speak(utterance);
 
 }
+
